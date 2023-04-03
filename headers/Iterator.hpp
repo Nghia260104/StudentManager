4f4203e453f7c92e7800fa10138aa4ca2deb2aea
Mab-Nig <maivuphong4@gmail.com>
2023-04-03 12:33:14 +0700

    Modify Iterator operator->. Add new header files.

--- headers/Iterator.hpp
+++ headers/Iterator.hpp
@@ -12,15 +12,14 @@ public:
 	Iterator();
 	Iterator(Node<T>*);
 	Iterator(const Iterator<T>&);
-	/* Iterator(const RIterator<T>&); */
 
 	Iterator<T>& operator=(const Iterator<T>&);
-	/* Iterator<T>& operator=(const RIterator<T>&); */
 	bool operator==(const Iterator<T>&) const;
 	bool operator!=(const Iterator<T>&) const;
 	int operator-(const Iterator<T>&) const;
 
 	T& operator*() const;
+	T* operator->() const;
 	Node<T>* getPointer() const;
 	
 	virtual Iterator<T>& operator++();
