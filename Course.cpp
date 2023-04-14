a0b5e0f1c3ad005f6f4b243044f2efbc23010fe7
Mab-Nig <maivuphong4@gmail.com>
2023-04-14 22:25:06 +0700

    Remove addStudent and add maximum number of students in a course.

--- Course.cpp
+++ Course.cpp
@@ -4,7 +4,7 @@
 using namespace Backend;
 
 Course::Course()
-	: semester_(nullptr)
+	: semester_(nullptr), maxStudents_(DEFAULT_MAX_STUDENTS)
 {}
 
 Course::Course(const std::string &nID)
@@ -18,6 +18,11 @@ const std::string& Course::getID() const
 	return id_;
 }
 
+int Course::getMaxStudents() const
+{
+	return maxStudents_;
+}
+
 const List<Student*>& Course::getStudents() const
 {
 	return students_;
@@ -28,9 +33,14 @@ void Course::setID(const std::string &nID)
 	id_ = nID;
 }
 
+void Course::setMaxStudents(int nMaxStudents)
+{
+	maxStudents_ = nMaxStudents;
+}
+
 bool Course::addStudent(Student *nStudent)
 {
-	if (students_.find(nStudent))
+	if (students_.size() == maxStudents_ || tudents_.find(nStudent))
 	{
 		return 0;
 	}
