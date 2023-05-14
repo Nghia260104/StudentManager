135331dcb5095d7ed7ec12f82d20e8f7bbba2f12
River <phamtrungnghia2004@gmail.com>
2023-05-14 15:03:35 +0700

    FrontendGlobal

--- FrontendGlobal.cpp
+++ FrontendGlobal.cpp
@@ -7,8 +7,8 @@
 sf::RenderWindow window;
 sf::Clock Clock;
 sf::Font RegularFont, BoldFont, MediumFont, LightFont, HeavyFont, Helvetica, RecRobot, RecOpen, Alga, JB, Arial, ArialBold, Times;
-sf::Font TimesBold, Tahoma, Consola, Luvenia, Montelgo, Over;
-sf::Color BackgroundColor;
+sf::Font TimesBold, Tahoma, Consola, Luvenia, Montelgo, Over, IA, Calibri;
+sf::Color BackgroundColor, RightSideColor, ButtonColor, TextColor, ButtonCoverColor, RightSideCover, TextCoverColor;
 sf::Cursor cursor;
 LogIn LogInWindow;
 StudentWindow StudentScreen;
